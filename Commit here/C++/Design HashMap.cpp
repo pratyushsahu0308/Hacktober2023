eebe0342b8cc24e44161e9bb84b61c909ade7510
learner-prakharrr <126413965+learner-prakharrr@users.noreply.github.com>
2023-10-11 13:18:48 +0530

    Update Design HashMap.cpp
    make code look beautiful and easy to understand

--- Commit here/C++/Design HashMap.cpp
+++ Commit here/C++/Design HashMap.cpp
@@ -4,12 +4,15 @@ public:
     MyHashMap() {
         fill(data, data + 1000000, -1);
     }
+
     void put(int key, int val) {
         data[key] = val;
     }
+
     int get(int key) {
         return data[key];
     }
+
     void remove(int key) {
         data[key] = -1;
     }
